222be946be8434714874a4a2e2a1e9e9d6b9c4d8
Thiago Sol <thiagosol@ymail.com>
2021-09-04 12:40:39 -0300

    Atualização swagger

--- src/main/java/com/advocacy/advocacysystem/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/advocacy/advocacysystem/infrastructure/config/SwaggerConfig.java
@@ -4,20 +4,19 @@ import com.advocacy.advocacysystem.entrypoint.dto.PageableDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
-import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -33,11 +32,12 @@ public class SwaggerConfig {
                 .build()
                 .directModelSubstitute(Pageable.class, PageableDTO.class)
                 .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()));
+                .securitySchemes(Arrays.asList(apiKey()))
+                .apiInfo(apiInfo());
     }
 
 
-    private ApiKey apiKey() {
+    private SecurityScheme apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
     }
 
@@ -50,6 +50,14 @@ public class SwaggerConfig {
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
         return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Advocacy - REST API")
+                .description("Advocacy is an API that controls all the steps of a law firm, with regard to the registration of clients, lawyers, processes, and finance.")
+                .version("1.0.0")
+                .build();
     }
 
 }
